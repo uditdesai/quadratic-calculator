5f7cd5f75b8cb0825de46d0a78c7cfa977af6cf9
Udit Desai <udit.desai3@gmail.com>
2015-12-28 23:54:49 +0000

    fix no answer error

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*
-
-var a = prompt("What is the a value?");
-var b = prompt("What is the b value?");
-var c = prompt("What is the c value?");
-
-a = parseFloat(a);
-b = parseFloat(b);
-c = parseFloat(c);
-
-var insideSquareRoot = (a * c * -4);
-var fullInsideSquareRoot = (Math.pow(b, 2) + insideSquareRoot);
-var squareRoot = (Math.sqrt(fullInsideSquareRoot));
-var negativeBValue = (0 - b);
-var numeratorPlus = (negativeBValue + squareRoot);
-var denominator = (2 * a);
-var answerPlus = (numeratorPlus / denominator);
-alert(answerPlus);
-var numeratorNegative = (negativeBValue - squareRoot);
-var answerNegative = (numeratorNegative / denominator);
-alert(answerNegative);
-
-*/
-
 document.getElementById("submit").onclick = function() {
     
     var a = $("#avalue").val();
@@ -57,6 +33,8 @@ document.getElementById("submit").onclick = function() {
     if ( isNaN(answerPlus, answerNegative) ) {
         
         document.getElementById("xvaluefirst").innerHTML = "no answer :(";
+        
+        document.getElementById("xvaluesecond").innerHTML = "";
     
     } else {
     
